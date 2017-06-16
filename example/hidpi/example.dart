ff17426c396a318ae5ad284c183874c71f334be7
Bernhard <bernhard.robert.pichler@gmail.com>
2017-06-16 09:22:29 +0200

    Extended hidpi example

--- example/hidpi/example.dart
+++ example/hidpi/example.dart
@@ -17,7 +17,7 @@ Future main() async {
   StageXL.stageOptions.backgroundColor = Color.DarkSlateGray;
   StageXL.stageOptions.stageScaleMode = StageScaleMode.NO_SCALE;
   StageXL.bitmapDataLoadOptions.pixelRatios = <double>[1.0, 2.0, 3.0];
-  StageXL.bitmapDataLoadOptions.pixelRatios = <double>[1.0];
+  //StageXL.bitmapDataLoadOptions.pixelRatios = <double>[1.0];
 
   // Init Stage and RenderLoop
 
@@ -26,9 +26,25 @@ Future main() async {
   var renderLoop = new RenderLoop();
   renderLoop.addStage(stage);
 
-  // load BitmapFont
+  // load BitmapFont from File (default)
 
   var bitmapFont = await BitmapFont.load("font/font@1x.fnt");
+
+  // load BitmapFont from BitmapData (alternative)
+
+  /*
+  var resourceManager = new ResourceManager();
+  resourceManager.addBitmapData("fontPng", "font/font@1x.png");
+  resourceManager.addTextFile("font@1", "font/font@1x.fnt");
+  resourceManager.addTextFile("font@2", "font/font@2x.fnt");
+  resourceManager.addTextFile("font@3", "font/font@3x.fnt");
+  await resourceManager.load();
+
+  var fontBitmapData = resourceManager.getBitmapData("fontPng");
+  var fontPixelRatio = fontBitmapData.renderTextureQuad.pixelRatio.toStringAsFixed(0);
+  var fontDefinition = resourceManager.getTextFile("font@$fontPixelRatio");
+  var bitmapFont = await BitmapFont.fromBitmapData(fontBitmapData, fontDefinition);
+  */
 
   // create BitmapText and add it to the Stage
 
